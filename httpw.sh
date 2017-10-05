833bc67c41daa051a4da1e4e3fcebeaff7223b17
masakatsu iida <iida.masa@cntsv.jp>
2017-10-05 15:53:31 +0900

    bug fix httpw.sh

--- httpw.sh
+++ httpw.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 # Funtion : メモリ使用量と空き容量、起動しているhttpdの数、
-            ポート別のセッションの数の変動を記録する
+#            ポート別の接続状態のセッションの数の変動を記録する
 
-Date=`date '+%Y/%m/%d-%H:%M:%S'`  # EXECの読む時の形でない不融合起きる
+Date=`date '+%Y/%m/%d-%H:%M:%S'`  # EXECの形
 Mem=,`free | grep -E '\-/\+ buffers/cache:' | awk '{print $3, $4}'`)
 Httpd=`ps -ef | grep -E '^apache.*httpd$' | wc -l`
 
-Post80=`netstat -ant | grep ":80 " | grep ESTA | wc -l`
-Post80_all=`netstat -ant | grep ":80 " | wc -l`
+#HTTP :: xxx.xxx.xxx.xxx:80 ESTABLISHED
+Port80=`netstat -ant | grep ":80 " | grep ESTA | wc -l`
 
+#SSL :: xxx.xxx.xxx.xxx:10100 ESTABLISHED
 PortSSL=`netstat -ant | grep ":10100 " | grep ESTA | wc -l`
-PortSSL_all=`netstat -ant | grep ":10100 " | wc -l`
 
-Connect_MySQL=`netstat -ant | grep ":3306 " | grep ESTA | wc -l`
-Connect_MySQL_all=`netstat -ant | grep ":3306 "  | wc -l`
+#MYSQL :: xxx.xxx.xxx.xxx:3306 ESTABLISHED
+PortMySQL=`netstat -ant | grep ":3306 " | grep ESTA | wc -l`
 
-echo "datetime  used-mem free-mem   httpd    Port80      PortSLL         MySQL
-echo "--------- --------- --------- -------- ---------   ------ ------   ----- -----"
-echo "$Date, ${Mem[0]}, ${Mem[1]}, $Httpd, $Port,$Port,  $Port $PortSll, $Connect_MySQL,$Connect_MySQL_all"
+echo "datetime  used-mem  free-mem  httpd    Port80    PortSLL    PortMySQL"
+echo "--------- --------- --------- -------- --------- ---------- ---------"
+echo "$Date, ${Mem[0]}, ${Mem[1]}, $Httpd,  $Port80,  $PortSSL,  $PortMySQL"
 
