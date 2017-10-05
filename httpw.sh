906d8aa8f9d68acba0e658cc4dce879d65a8c777
masakatsu iida <iida.masa@cntsv.jp>
2017-10-05 16:53:14 +0900

    bug fix httpw.sh

--- httpw.sh
+++ httpw.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
-# Funtion : メモリ使用量と空き容量、起動しているhttpdの数、
-#            ポート別の接続状態のセッションの数の変動を記録する
+# create : 2017/09/14 m.iida
+# update : 2017/09/15
+# update : 2017/09/20
 
-Date=`date '+%Y/%m/%d-%H:%M:%S'`  # EXECの形
-Mem=,`free | grep -E '\-/\+ buffers/cache:' | awk '{print $3, $4}'`)
-Httpd=`ps -ef | grep -E '^apache.*httpd$' | wc -l`
+DATE=`date '+%Y/%m/%d-%H:%M:%S'`
+MEM=(`free | grep -E '\-/\+ buffers/cache:' | awk '{print $3, $4}'`)
+PROC_CNT=`ps -ef | grep -E '^apache.*httpd$' | wc -l`
+CONNECT_80=`netstat -ant | grep ":80 " | grep ESTA | wc -l`
+CONNECT_80_ALL=`netstat -ant | grep ":80 " | wc -l`
+CONNECT_SSL=`netstat -ant | grep ":10100 " | grep ESTA | wc -l`
+CONNECT_SSL_ALL=`netstat -ant | grep ":10100 " | wc -l`
+CONNECT_MySQL=`netstat -ant | grep ":3306 " | grep ESTA | wc -l`
+CONNECT_MySQL_ALL=`netstat -ant | grep ":3306 "  | wc -l`
 
-#HTTP :: xxx.xxx.xxx.xxx:80 ESTABLISHED
-Port80=`netstat -ant | grep ":80 " | grep ESTA | wc -l`
-
-#SSL :: xxx.xxx.xxx.xxx:10100 ESTABLISHED
-PortSSL=`netstat -ant | grep ":10100 " | grep ESTA | wc -l`
-
-#MYSQL :: xxx.xxx.xxx.xxx:3306 ESTABLISHED
-PortMySQL=`netstat -ant | grep ":3306 " | grep ESTA | wc -l`
-
-echo "datetime  used-mem  free-mem  httpd    Port80    PortSLL    PortMySQL"
-echo "--------- --------- --------- -------- --------- ---------- ---------"
-echo "$Date, ${Mem[0]}, ${Mem[1]}, $Httpd,  $Port80,  $PortSSL,  $PortMySQL"
+echo "DATE      TIME      TOTAL-MEM FREE-MEM HTTPDs P80    SSL  MySQL"
+echo "--------- --------- --------- -------- ------ ------ ---- -----"
+echo "$DATE, ${MEM[0]}, ${MEM[1]}, $PROC_CNT, $CONNECT_80($CONNECT_80_ALL), $CONNECT_SSL($CONNECT_SSL_ALL), $CONNECT_MySQL($CONNECT_MySQL_ALL)"
 
