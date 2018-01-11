4da6a408ff5ab4812cf9ac25fd9e673985b64cd5
Iida masakatsu <miida@nssx.co.jp>
2018-01-11 20:15:03 +0900

    update cron_disk_use_check.sh grep_malware.sh

--- work/cron_disk_use_check.sh
+++ work/cron_disk_use_check.sh
@@ -1,32 +1,6 @@
-"cron_disk_use_check.sh" 29L, 641C                                                                                        2,1           Top
 #!/bin/bash
-# create : 2017/09/27
 
-Your_email_address='nos@nssx.co.jp'
-Limit=50
-
-function func_send_alert()
-{
-  echo -e " \
-Create by $0 at `date` \n \
-hostname : `hostname` \n \
-\n \
-Disk usage alert: $i %\n \
----- detail ---- \n \
-`/bin/df -h`" \
-  | /bin/mail -s "[WARNING][`hostname`] disk space alert" "$Your_email_address"
-}
-
-## Main process ##
-array=(`/bin/df | /bin/awk 'NR>1{gsub("%","",$5);print $5}'`)
-for i in "${array[@]}"
-do
-  if [ $i -gt $Limit ]; then
-"cron_disk_use_check.sh" 29L, 641C                                                                                        2,1           Top
-#!/bin/bash
-# create : 2017/09/27
-
-Your_email_address='nos@nssx.co.jp'
+Your_email_address='xxx@xxx.xx'
 Limit=80
 
 function func_send_alert()
