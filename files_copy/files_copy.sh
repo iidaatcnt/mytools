7fd10bd2d94310468b4c9419fc02c3039ab23dc5
Iida masakatsu <miida@nssx.co.jp>
2018-01-11 20:00:54 +0900

    update files_copy.sh

--- files_copy/files_copy.sh
+++ files_copy/files_copy.sh
@@ -1,24 +1,10 @@
 #/usr/bin/bash
-# create : 2017/11/21 m.iida
 
 lb=(
-/var/www/html/style-arena.jp/STG/style-arena.jp/public/images.2015-01-21.bak/areacontents/20131202/footer.php
-/var/www/html/style-arena.jp/STG/style-arena.jp/public/images.2015-01-21.bak/streetstyle/2009/8/4/javascript.php
-/var/www/html/style-arena.jp/STG/style-arena.jp/public/images.2015-01-21.bak/streetstyle/2013/5/4/.system60.php
-/var/www/html/style-arena.jp/STG/style-arena.jp/public/images.2015-01-21.bak/streetstyle/2014/10/3/.defines37.php
-/var/www/html/style-arena.jp/STG/style-arena.jp/public/images.2015-01-21.bak/master/page.php
-/var/www/html/style-arena.jp/STG/style-arena.jp/public/images.2015-01-21.bak/master/icon/shops.category/utf.php
-/var/www/html/style-arena.jp/STG/style-arena.jp/public/images.2015-01-21.bak/snaps/201407/include.php
-/var/www/html/style-arena.jp/STG/style-arena.jp/public/images.2015-01-21.bak/snaps/201403/admin.php
-/var/www/html/style-arena.jp/STG/style-arena.jp/public/images/areacontents/20131202/footer.php
-/var/www/html/style-arena.jp/STG/style-arena.jp/public/images/streetstyle/2009/8/4/javascript.php
-/var/www/html/style-arena.jp/STG/style-arena.jp/public/images/streetstyle/2013/5/4/.system60.php
-/var/www/html/style-arena.jp/STG/style-arena.jp/public/images/streetstyle/2014/10/3/.defines37.php
-/var/www/html/style-arena.jp/STG/style-arena.jp/public/images/master/page.php
-/var/www/html/style-arena.jp/STG/style-arena.jp/public/images/master/icon/shops.category/utf.php
-/var/www/html/style-arena.jp/STG/style-arena.jp/public/images/event/wp-booter.php
-/var/www/html/style-arena.jp/STG/style-arena.jp/public/images/snaps/201407/include.php
-/var/www/html/style-arena.jp/STG/style-arena.jp/public/images/snaps/201403/admin.php
+/var/www/html/STG/images/master/page.php
+/var/www/html/STG/images/event/wp-booter.php
+/var/www/html/STG/images/snaps/yymmdd/include.php
+/var/www/html/STG/images/snaps/yymmdd/admin.php
 )
 
 for i in ${lb[@]}; do
