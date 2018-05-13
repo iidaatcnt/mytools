cf49f75155e0fd08c378ed5879a1045bcfd01387
iida masakatsu <iida.masa@cntsv.jp>
2018-05-13 21:24:46 +0900

    del japanese char

--- Python_codes/jsons.py
+++ Python_codes/jsons.py
@@ -8,21 +8,21 @@ j = {
   }
 }
 
-# jsonを表示する
+#
 print("-" * 10)
 print(j)
 print("-" * 10)
 
-# jsonをダンプする
+#
 print(json.dumps(j))
 
 a = json.dumps(j)
 print(json.loads(a))
 
-# ファイルに書き込む
+#
 with open('test.json', 'w') as f:
     json.dump(j, f)
 
-# ファイルから読み込む
+#
 with open('test.json', 'r') as f:
     json.load(f)
