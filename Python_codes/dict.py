7cd3e391e8f1e25685e4bf0e0f8f3f13ca332f9c
iida masakatsu <iida.masa@cntsv.jp>
2018-05-15 07:32:32 +0900

    update json file

---
+++ Python_codes/dict.py
@@ -0,0 +1,17 @@
+d = {'x':10, 'y':20}
+
+print(d)
+
+print(d.keys())
+
+print(d.values())
+
+d2 = {'x':123,'z':456}
+
+print(d2)
+
+d.update(d2)
+
+print(d)
+
+print(d['x'])
---
+++ Python_codes/set.py
@@ -0,0 +1,21 @@
+a = {1,2,2,3,4,5,6,7}  # 2 duble
+
+print(a) # uniq
+
+print(type(a))
+
+a = {1,2,3,4,5,6}
+b = {2,3,3,5,7}
+
+
+print(a - b)
+
+print(b - a)
+
+print(a & b)
+
+print(a | b)
+
+print(a ^ b)
+
+# a + b は間違い
