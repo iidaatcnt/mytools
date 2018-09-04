68764a0f3f03432bfd33e7c388503b56626753e3
iida masakatsu <iida.masa@cntsv.jp>
2018-09-05 07:28:35 +0900

    add pandas

---
+++ Python_codes/pandas01.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+s = pd.Series([1,2,3])
+
+print(s)
+# 0    1
+# 1    2
+# 2    3
+
+print('s[0] = ',s[0])
+# s[0] =  1
+
+print('s.sum() = ', s.sum())
+# s.sum() =  6
+
+df = pd.DataFrame({'A':[1,2], 'B':[3,4]})
+print(df)
+#    A  B
+# 0  1  3
+# 1  2  4
--- Python_codes/set.py
+++ Python_codes/set.py
@@ -1,29 +1,9 @@
-a = {1,2,2,3,4,5,6,7}  # 2 duble
-
-print(a) # uniq
-
-print(type(a))
-
-a = {1,2,3,4,5,6}
-b = {2,3,3,5,7}
-
-
-print(a - b)
-
-print(b - a)
-
-print(a & b)
-
-print(a | b)
-
-print(a ^ b)
-
-# a + b は間違い
-print('--')
-print(a.add(8))
-
-print(a.remove(1))
-
-print(a.clear())
-
-
+def ppap():
+    '''
+    pen
+    apple
+    pineapple
+    pen
+    PPAP
+    '''
+print('test') # test desu.
