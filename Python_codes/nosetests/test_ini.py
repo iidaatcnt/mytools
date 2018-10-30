102bdb76428980754f7128d379cc75466afedab4
Masa Iida <iida.m@cntsv.jp>
2018-10-30 09:37:45 +0900

    add nosetests/

---
+++ Python_codes/nosetests/test_ini.py
@@ -0,0 +1,13 @@
+import nose
+
+# test_int.py
+# assert 文をシンプルに書ける ok_ eq_ メソッドの読み込み
+from nose.tools import ok_, eq_
+
+# テストケース
+# test で始まる名前の関数で書けばOK
+def test_1_is_true():
+    ok_(bool(1))
+
+def test_1_plus_2():
+    eq(1 + 2, 3)
